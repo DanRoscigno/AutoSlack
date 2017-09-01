89283812e4edf67ac26fddae863530d11b251d02
DanRoscigno <dan@roscigno.com>
2017-09-01 12:35:07 -0400

    SQL for the needed tables.  Note: There are more rows, but I filtered out customer names.

--- freshRow.sql
+++ freshRow.sql
@@ -1,3 +1,5 @@
+delete from alerts.applicationMap;
+go
 create table alerts.freshRow_events persistent
 (
         Identifier      varchar(255)    primary key,
@@ -13,4 +15,16 @@ create table alerts.applicationMap persistent
         OpsTeam         varchar(8),
         Service         varchar(8)
 );
+go
+insert into alerts.applicationMap values ('APM','APM','APM');
+insert into alerts.applicationMap values ('FEDERAL_APM','APM','APM');
+insert into alerts.applicationMap values ('IntegrationBackplane','Storage','IB');
+insert into alerts.applicationMap values ('Maximo','ICD','ICD');
+insert into alerts.applicationMap values ('PI','APM','Predict');
+insert into alerts.applicationMap values ('PredictivInsight','APM','Predict');
+insert into alerts.applicationMap values ('SCCD','ICD','ICD');
+insert into alerts.applicationMap values ('StorageAnalytics','Storage','Storage');
+insert into alerts.applicationMap values ('TPC','Storage','Storage');
+insert into alerts.applicationMap values ('TWS','Workload','Workload');
+insert into alerts.applicationMap values ('WA','Workload','Workload');
 go
